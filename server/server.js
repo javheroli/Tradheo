83461a3715e2e85823e64f3ff9dfc5138f5feaee
Unknown <javierherraizolivas@gmail.com>
2019-06-26 19:39:25 +0200

    #4 [Back-end]: Added API's body parsers

--- server/server.js
+++ server/server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 require('dotenv').config();
+var bodyParser = require('body-parser');
+var multer = require('multer');
+const cloudinary = require("cloudinary");
+const cloudinaryStorage = require("multer-storage-cloudinary");
 
 //Connection to DataBase:
 //To connect to Development environment DB (Comment line below if not using it)
@@ -16,6 +20,34 @@ mongoose.connection.on('error', error => console.log(error));
 mongoose.Promise = global.Promise;
 
 const app = express();
+
+//For parsing body with Content-Type application/json
+app.use(bodyParser.json());
+//For parsing body with Content-Type application/xwww-form-urlencoded
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+//For parsing multipart/form-data
+//Parser to upload an image to Cloudinary
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET
+});
+const storage = cloudinaryStorage({
+  cloudinary: cloudinary,
+  folder: "userImages",
+  allowedFormats: ["jpg", "png", "jpeg", "gif"],
+  transformation: [{
+    width: 500,
+    height: 500,
+    crop: "limit"
+  }]
+});
+const parser = multer({
+  storage: storage
+});
+app.use(parser.single('image'));
 
 //Allowing Cross-Origin Request
 app.use((req, res, next) => {
